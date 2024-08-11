f78eb7ab67f022bd96830d516fbc45d2f01ee91a
Trịnh Thanh Sơn <thanhson12a12002@gmail.com>
2024-08-11 08:51:12 +0700

    DD

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import About from '../pages/About/About'
 import Origin from '../pages/About/Origin'
