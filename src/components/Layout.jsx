9b74b5918b26ba75de7146714cca2c0742ad51af
Trịnh Thanh Sơn <thanhson12a12002@gmail.com>
2024-08-11 08:46:08 +0700

    DD

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,31 @@
+import { useLocation } from 'react-router-dom'
 import Navbar from './Navbar.jsx'
 import Footer from './Footer.jsx'
 import Routers from '../routers/Routers.jsx'
 
 const Layout = () => {
+  const location = useLocation()
+
+  const isAdminPage = location.pathname.startsWith('/manager')
+  const isLoginPage = location.pathname === '/'
+  const isSignupPage = location.pathname === '/signup'
+  const isForgotPasswordPage = location.pathname === '/forgotPassword'
+  const isIndexPage = location.pathname === '/'
+
+  if (
+    isIndexPage ||
+    isLoginPage ||
+    isSignupPage ||
+    isForgotPasswordPage ||
+    isAdminPage
+  ) {
+    return (
+      <div>
+        <Routers />
+      </div>
+    )
+  }
+
   return (
     <div>
       <Navbar />
