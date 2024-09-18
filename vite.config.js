466c505c6bc24a2daa4ccaa59b51a05bd72eb367
Trịnh Thanh Sơn <thanhson12a12002@gmail.com>
2024-09-18 16:56:39 +0700

    g

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: false,  // Sử dụng HTTP thay vì HTTPS trong môi trường dev
     proxy: {
       '/api': {
         target: 'http://3.25.162.185:8080/',
